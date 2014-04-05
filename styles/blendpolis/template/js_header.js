fabdc21d7805f42c819d06a38eaf3c448bbc8a21
Julian Herzog <Julian.Hzg@gmail.com>
2014-04-05 14:24:51 +0200

    Adding changes in themes that already existed before, remove .svn-folders.

---
+++ styles/blendpolis/template/js_header.js
@@ -0,0 +1,40 @@
+var onload_functions = new Array();
+
+/**
+* Alternate display of subPanels
+*/
+function subPanels(p)
+{
+	var i, e, t;
+
+	if (typeof(p) == 'string')
+	{
+		show_panel = p;
+	}
+
+	for (i = 0; i < panels.length; i++)
+	{
+		e = document.getElementById(panels[i]);
+		t = document.getElementById(panels[i] + '-tab');
+
+		if (e)
+		{
+			if (panels[i] == show_panel)
+			{
+				e.style.display = 'block';
+				if (t)
+				{
+					t.className = 'activetab';
+				}
+			}
+			else
+			{
+				e.style.display = 'none';
+				if (t)
+				{
+					t.className = '';
+				}
+			}
+		}
+	}
+}
